dc46d7638d0b515c7c167f7d903c846358069d82
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2019-11-19 20:58:39 -0500

    Defined call method with inital dialogue

--- lib/hogwarts/cli.rb
+++ lib/hogwarts/cli.rb
@@ -1,4 +1,16 @@
 class CLI
 
+    def call
+        puts "Welcome, young wizard."
+        sleep 2
+        puts "When I call your name, you will come forth."
+        sleep 2
+        puts "I shall place the sorting hat on your head."
+        sleep 2
+        puts "And you will be sorted into your houses."
+        sleep 2
+        puts "Ah, right then..."
+        Api.new.random_house
+    end
 
 end
