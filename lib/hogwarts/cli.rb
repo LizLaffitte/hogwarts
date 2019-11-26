7f0c519a9852f6d5123c2f7fdc9d5e96ae6c1a6a
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2019-11-26 10:53:49 -0500

    Trying to write conditional to handle incorrect user error.

--- lib/hogwarts/cli.rb
+++ lib/hogwarts/cli.rb
@@ -13,28 +13,32 @@ class CLI
         sleep 1
         puts "Hmm...difficult, very difficult..."
         puts "Where to put you?"
-        user_house
-        puts "There are four houses at the Hogwarts School of Witchcraft and Wizardry."
-        puts "Enter the name (or number) of a house to learn more about it,"
-        puts "or enter 'exit' to leave Hogwarts."
-        @your_api_instance.houses
-        list_houses
-        @input_house = gets.chomp.capitalize
-        #puts "Excellent! Ten points to #{@user_house}!"
         sleep 2
+        user_house
+        input = nil
         while input != "exit"
-            input = list_top_menu_options
-            case input
-            when "house info"
-                house_info
-                puts "Where to now, brave young wizard?"
-                input = gets.chomp
-            when "change house"
-                puts "change house"
-            else
-                "Have you been confunded? Try again."
+            list_houses
+            input = gets.chomp.capitalize
+            if House.find_house(input) == nil
+                puts "Have you been confunded? Try again."
+                break
+            else 
+                house_info(input)
             end
         end
+        #puts "Excellent! Ten points to #{@user_house}!"
+        # while input != "exit"
+        #     case input
+        #     when "house info"
+        #         house_info
+        #         puts "Where to now, brave young wizard?"
+        #         input = gets.chomp
+        #     when "change house"
+        #         puts "change house"
+        #     else
+        #         "Have you been confunded? Try again."
+        #     end
+        # end
     end
 
     def user_house
@@ -44,24 +48,27 @@ class CLI
         sleep 1
     end
 
-    def list_top_menu_options
-        puts "To learn more about your house, enter 'house info'."
-        #puts "To see what other students are in your house, enter 'housemates'."
-        #puts "To cast a spell, enter 'spells'."
-        puts "To change your house, enter 'change house'."
-        puts "To quit, type 'exit'."
-        puts "What would you like to do?"    
-        input = gets.chomp.downcase
-    end
+    # def list_top_menu_options
+    #     puts "To learn more about your house, enter 'house info'."
+    #     #puts "To see what other students are in your house, enter 'housemates'."
+    #     #puts "To cast a spell, enter 'spells'."
+    #     puts "To change your house, enter 'change house'."
+    #     puts "To quit, type 'exit'."
+    #     puts "What would you like to do?"    
+    #     input = gets.chomp.downcase
+    # end
 
     def list_houses
+        puts "There are four houses at the Hogwarts School of Witchcraft and Wizardry."
+        puts "Enter the name of a house to learn more about it,"
+        puts "or enter 'exit' to leave Hogwarts."
+        @your_api_instance.houses
         House.all.sort_by(&:name).each.with_index(1){|house, idx| puts "#{idx}. #{house.name}"}
     end
 
-    def house_info
+    def house_info(house)
         my_house = House.find_house(@input_house)
         my_house.list_house_info
-
     end
 
 end
