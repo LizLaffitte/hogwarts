80e8cf49d48d543a0108a6015abbb0c4b8d6ce58
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2019-12-02 20:50:33 -0500

    Changing #character_info to enumerate through character object's instance variables and putsing out to CLI.

--- lib/hogwarts/character.rb
+++ lib/hogwarts/character.rb
@@ -18,16 +18,28 @@ class Character
     end
 
     def character_info
-        self.patronus ||= "Unknown"
-        self.wand ||= "Unknown"
-        self.boggart ||= "Unknown"
-        self.role ||= "Unknown"
-        puts "Member: #{@name}"
-        puts "House: #{self.house.name}"
-        puts "Status: #{@role.capitalize}"
-        puts "Patronus: #{@patronus.capitalize}"
-        puts "Boggart: #{@boggart.capitalize}"
-        puts "Wand: #{@wand.capitalize}"
+        #Only want to display an attribute if it has been defined & isn't unknown.
+        #self.patronus ||= "Unknown"
+        # self.wand ||= "Unknown"
+        # self.boggart ||= "Unknown"
+        # self.role ||= "Unknown"
+        # puts "Member: #{@name}"
+        # puts "House: #{self.house.name}"
+        # puts "Status: #{@role.capitalize}"
+        # puts "Patronus: #{@patronus.capitalize}"
+        # puts "Boggart: #{@boggart.capitalize}"
+        # puts "Wand: #{@wand.capitalize}"
+        self.instance_variables.each do |inst_var| 
+            unless inst_var == :@id || instance_variable_get(inst_var) == nil
+                characteristic = inst_var.to_s.gsub("@", "").capitalize + ": "
+                if inst_var == :@house
+                    characteristic += "#{self.house.name}"
+                else
+                characteristic += instance_variable_get(inst_var).to_s
+                end
+                puts characteristic
+            end
+        end
     end
     
 end
